56be97475edc433211acfaa242895c935e9f0a34
Yong Pok Low <yongpok95@live.nl>
2023-11-10 19:59:43 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_basics
+{
+    class program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World");
+            Console.Beep();
+            // This is a comment //
+            Console.ReadKey();
+        }
+    }
+}
