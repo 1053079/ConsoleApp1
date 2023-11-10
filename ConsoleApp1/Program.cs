0e8f669bcedd44cc487672a379673ed3d490e4c1
Yong Pok Low <yongpok95@live.nl>
2023-11-10 20:13:26 +0100

    Just testing

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,10 +9,16 @@ namespace CS_basics
     class program
     {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World");
-            Console.Beep();
-            // This is a comment //
-            Console.ReadKey();
+            int x = 123; 
+            int y = 377;
+            int z = x + y; // x + y 
+
+            int age = 500; // you are 500 years old
+             
+            Console.WriteLine("x and y together is " + z); // prints z to the console
+            Console.WriteLine("You are " + age + " years old"); // writes to console your age
+
+            Console.ReadKey(); // no ugly text blocking you in the console
         }
     }
 }
