59b4b72ecf25ff9d21d31aa6c698a256a5f13c15
Yong Pok Low <yongpok95@live.nl>
2023-11-10 21:26:17 +0100

    rwewe

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,10 +14,20 @@ namespace CS_basics
             int z = x + y; // x + y 
 
             int age = 500; // you are 500 years old
+            double height = 1.78; // Your height. declaring it a double is used for numbers with decimals.
+            bool alive = true; // You are not dead, I think.
+            char symbol = '%'; // char allows only one character 
+            String name = "Kunigami"; 
              
             Console.WriteLine("x and y together is " + z); // prints z to the console
             Console.WriteLine("You are " + age + " years old"); // writes to console your age
+            Console.WriteLine("Your height is " + height + " meters"); // your height on the console
+            Console.WriteLine("You are alive? " + alive);
+            Console.WriteLine("Your symbol is " + symbol);
+            Console.WriteLine("Your name is " + name);
 
+            int b = Convert.ToInt32(height);
+            Console.WriteLine(b);
             Console.ReadKey(); // no ugly text blocking you in the console
         }
     }
